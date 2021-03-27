12bf7504c624bf8fae03b42ce25066bb32937976
Jatin Sadhwani <75443345+JatinSadhwani02@users.noreply.github.com>
2021-03-27 13:09:58 +0530

    app.py

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def predict():
     return render_template('index.html',prediction_text="Number of Weekly Rides Should be {}".format(math.floor(output)))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port='8000')
+    app.run(debug=True)
